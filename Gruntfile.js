f03e8d147283306825843edc3cbd70da8af1af5b
Morteza Ansarinia <ansarinia@me.com>
2013-10-10 02:55:43 +0330

    Initial commit for the new theme-based version.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,127 +1,96 @@
-/* jshint node: true */
 module.exports = function(grunt) {
-    "use strict";
+  "use strict";
 
-    // Project configuration.
-    grunt.initConfig({
-        // Metadata.
-        pkg: grunt.file.readJSON('package.json'),
-        banner: '/**\n' +
-                '* <%= pkg.name %>.js v<%= pkg.version %> by @fat and @mdo\n' +
-                '* Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
-                '* <%= _.pluck(pkg.licenses, "url").join(", ") %>\n' +
-                '*/\n',
-        jqueryCheck: 'if (!jQuery) { throw new Error(\"Bootstrap requires jQuery\") }\n\n',
-        // Task configuration.
-        clean: {
-            dist: ['dist']
+  // Project configuration.
+  grunt.initConfig({
+
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner:   '/*******************************************************************************\n' +
+              '*        File: bootstrap-rtl.css\n' +
+              '*      Author: <%= pkg.author %>\n' +
+              '*  Created on: <%= grunt.template.today("mmmm dd,yyyy") %>\n' +
+              '*     Project: <%= pkg.name %>\n' +
+              '*   Copyright: See the file "LICENSE" for the full license governing this code.\n' +
+              '*******************************************************************************/\n\n',
+    jqueryCheck: 'if (typeof jQuery === "undefined") { throw new Error("Bootstrap requires jQuery") }\n\n',
+
+    // Task configuration.
+    clean: {
+      dist: ['dist']
+    },
+
+    recess: {
+      options: {
+        compile: true,
+        banner: '<%= banner %>'
+      },
+      rtl: {
+        src: ['less/bootstrap-rtl.less'],
+        dest: 'dist/css/<%= pkg.name %>.css'
+      },
+      min: {
+        options: {
+          compress: true
         },
-        concat: {
-            options: {
-                banner: '<%= banner %><%= jqueryCheck %>',
-                stripBanners: false
-            },
-            bootstrap: {
-                src: ['js/transition.js', 'js/alert.js', 'js/button.js', 'js/carousel.js', 'js/collapse.js', 'js/dropdown.js', 'js/modal.js', 'js/tooltip.js', 'js/popover.js', 'js/scrollspy.js', 'js/tab.js', 'js/affix.js'],
-                dest: 'dist/js/<%= pkg.name %>.js'
-            }
+        src: ['less/bootstrap-rtl.less'],
+        dest: 'dist/css/<%= pkg.name %>.min.css'
+      },/*
+      theme: {
+        src: ['less/theme.less'],
+        dest: 'dist/css/<%= pkg.name %>-theme.css'
+      },
+      theme_min: {
+        options: {
+          compress: true
         },
-        jshint: {
-            options: {
-                jshintrc: 'js/.jshintrc'
-            },
-            gruntfile: {
-                src: 'Gruntfile.js'
-            },
-            src: {
-                src: ['js/*.js']
-            },
-            test: {
-                src: ['js/tests/unit/*.js']
-            }
-        },
-        recess: {
-            options: {
-                compile: true
-            },
-            bootstrap: {
-                files: {
-                    'dist/css/bootstrap-rtl.css': ['less/bootstrap.less']
-                }
-            },
-            min: {
-                options: {
-                    compress: true
-                },
-                files: {
-                    'dist/css/bootstrap-rtl.min.css': ['less/bootstrap.less']
-                }
-            }
-        },
-        uglify: {
-            options: {
-                banner: '<%= banner %>'
-            },
-            bootstrap: {
-                files: {
-                    'dist/js/<%= pkg.name %>.min.js': ['<%= concat.bootstrap.dest %>']
-                }
-            }
-        },
-        qunit: {
-            options: {
-                inject: 'js/tests/unit/phantom.js'
-            },
-            files: ['js/tests/*.html']
-        },
-        connect: {
-            server: {
-                options: {
-                    port: 3000,
-                    base: '.'
-                }
-            }
-        },
-        watch: {
-            src: {
-                files: '<%= jshint.src.src %>',
-                tasks: ['jshint:src', 'qunit']
-            },
-            test: {
-                files: '<%= jshint.test.src %>',
-                tasks: ['jshint:test', 'qunit']
-            },
-            recess: {
-                files: 'less/*.less',
-                tasks: ['recess']
-            }
-        }
-    });
+        src: ['less/theme.less'],
+        dest: 'dist/css/<%= pkg.name %>-theme.min.css'
+      }*/
+
+    },
+
+    copy: {
+      fonts: {
+        expand: true,
+        src: ["assets/fonts/*"],
+        flatten: true,
+        dest: 'dist/fonts/'
+      },
+      js: {
+        expand: true,
+        src: ["assets/js/*"],
+        flatten: true,
+        dest: 'dist/js/'
+      },
+      css: {
+        expand: true,
+        src: ["assets/css/*"],
+        flatten: true,
+        dest: 'dist/css/'
+      }
+    }
+
+  });
 
 
-    // These plugins provide necessary tasks.
-    grunt.loadNpmTasks('grunt-contrib-connect');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-qunit');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-recess');
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  //grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-recess');
 
+  // CSS distribution task.
+  grunt.registerTask('dist-css', ['recess']);
 
-    // Test task.
-    grunt.registerTask('test', ['jshint', 'qunit']);
+  // Assets distribution task.
+  grunt.registerTask('dist-assets', ['copy']);
 
-    // JS distribution task.
-    grunt.registerTask('dist-js', ['concat', 'uglify']);
+  // Full distribution task.
+  grunt.registerTask('dist', ['clean', 'dist-assets', 'dist-css']);
 
-    // CSS distribution task.
-    grunt.registerTask('dist-css', ['recess']);
+  // Default task.
+  grunt.registerTask('default', ['dist']);
 
-    // Full distribution task.
-    grunt.registerTask('dist', ['clean', 'dist-css', 'dist-js']);
-
-    // Default task.
-    grunt.registerTask('default', ['test', 'dist']);
 };
--- README.md
+++ README.md
@@ -1,13 +1,45 @@
-Bootstrap 3 RTL
-================
+# Bootstrap RTL
 
-Right-to-Left version of Bootstrap 3 for farsi and other RTL languages.
+> Bootstrap is a sleek, intuitive, and powerful front-end framework for faster and easier web development.
 
-It is not ready yet. Please use at your own discretion.
+Bootstrap RTL provides simple yet complete right-to-left capability for Bootstrap 3. Simply add `bootstrap-rtl.min.css` to your pages.
 
-Screenshots
-================
+To get started, check out `examples/` directory.
 
-![Screenshot #1](./screenshots/bt3-rtl-2.png "Bootstrap3 RTL Screenshot #1")
 
-![Screenshot #2](./screenshots/bt3-rtl-1.png "Bootstrap3 RTL Screenshot #2")
+## Quick start
+
+In addition to bootstrap distribution, LESS codes for RTL theme and compiled CSS (`bootstrap-rtl.css`) are provided, as well as minified CSS (`bootstrap-rtl.min.css`). Check `dist/` directory for all the codes you need.
+
+#### Fonts
+*Glyphicons* and *Iranian Sans* are included for icons and farsi scripts, respectively.
+
+
+## Bugs and feature requests
+
+To report a bug or request a feature [please open a new issue](https://github.com/morteza/bootstrap-rtl/issues).
+
+## Compiling CSS and JavaScript
+
+Bootstrap RTL uses [Grunt](http://gruntjs.com/) for working with the framework. Follow these simple steps to prepare and compile:
+
+1. Run `npm install` to download and prepare dependencies.
+2. Run `grunt`, and check `dist/` directory for outputs.
+
+
+## Author
+
+**Morteza Ansarinia**
+
++ [http://github.com/morteza](http://github.com/morteza)
+
+
+## Copyright and license
+
+
+#### Bootstrap RTL
+See the file [LICENSE](LICENSE) for the full license governing this project.
+
+#### Bootstrap
+&copy; 2013 Twitter, Inc. Under [the Apache 2.0 license](LICENSE.bootstrap).
+
